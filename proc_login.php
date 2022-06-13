4cbd0aad6fb59f18cc03003c15327f78da3d7161
caio-viana <54335844+caio-viana@users.noreply.github.com>
2022-06-13 13:20:46 -0300

    Added link to login page

--- proc_login.php
+++ proc_login.php
@@ -15,12 +15,13 @@ if($conn->connect_error){
     die("Connection failed". $connect_error);
 
 }
-
-
-
+ //query
 $sql = "SELECT * FROM tb_cadastro where email='$email' and senha='$senha'";
+
+//armazena resultado da query
 $result = $conn->query($sql);
 
+//checa se tem retorno no select, se tiver, login realizado
 if ($result->num_rows > 0){
     echo "login realizado";
 } else {
